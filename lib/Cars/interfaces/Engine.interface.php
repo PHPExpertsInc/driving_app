727a11029993fd500260819579d28f75fe8a4026
hopeseekr <hopeseekr@xmule.ws>
2010-08-28 22:03:07 -0500

    Add the licensing information.

--- lib/Cars/interfaces/Engine.interface.php
+++ lib/Cars/interfaces/Engine.interface.php
@@ -1,4 +1,19 @@
 <?php
+/**
+* Driving App
+*   Copyright © 2010 Theodore R. Smith <theodore@phpexperts.pro>
+* 
+* The following code is licensed under a modified BSD License.
+* All of the terms and conditions of the BSD License apply with one
+* exception:
+*
+* 1. Every one who has not been a registered student of the "PHPExperts
+*    From Beginner To Pro" course (http://www.phpexperts.pro/) is forbidden
+*    from modifing this code or using in an another project, either as a
+*    deritvative work or stand-alone.
+*
+* BSD License: http://www.opensource.org/licenses/bsd-license.php
+**/
 
 interface Engine
 {
