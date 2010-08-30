be7d6c28f58aeedb1f36033122361ab22fd06e0b
hopeseekr <hopeseekr@xmule.ws>
2010-08-29 20:10:10 -0500

    Convert command line options to _GET parameters.

--- lib/Cars/lib/misc.inc.php
+++ lib/Cars/lib/misc.inc.php
@@ -52,6 +52,15 @@ function attemptAction($class, $action, $actor, $args = null)
     }
 }
 
+function convert_command_line_to_get()
+{
+    if (php_sapi_name() == 'cli')
+    {
+        $_GET = getopt('', array('debug::'));
+        print_r($_GET);
+    }    
+}
+
 function set_debug_level()
 {
     if (defined('DEBUG'))
@@ -59,27 +68,14 @@ function set_debug_level()
         return;
     }
 
-    // Figure out if we're running from the command line or a web server.
-    if (php_sapi_name() == 'cli')
+    if (isset($_COOKIE['debug']))
     {
-        $args = getopt('', array('debug::'));
-
-        if (isset($args['debug']))
-        {
-            $debug_level = $args['debug'];
-        }
+        $debug_level = filter_input(INPUT_COOKIE, 'debug', FILTER_SANITIZE_NUMBER_INT);
     }
-    else
-    {
-        if (isset($_COOKIE['debug']))
-        {
-            $debug_level = filter_input(INPUT_COOKIE, 'debug', FILTER_SANITIZE_NUMBER_INT);
-        }
 
-        if (isset($_GET['debug']))
-        {
-            $debug_level = filter_input(INPUT_GET, 'debug', FILTER_SANITIZE_NUMBER_INT);
-        }
+    if (isset($_GET['debug']))
+    {
+        $debug_level = filter_var($_GET['debug'], FILTER_SANITIZE_NUMBER_INT);
     }
 
     if (!isset($debug_level))
